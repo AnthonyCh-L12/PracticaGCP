fad67840846cc4a49f9e2a5bba6ea1645958f7a1
AnthonyCh-L12 <155906738+AnthonyCh-L12@users.noreply.github.com>
2024-11-06 11:31:35 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "aasd",
-"autor" => "asdas",
-"resumen" => "asd",
+"titulo" => "CIENCIA",
+"autor" => "Anthony Chávez Lujan",
+"resumen" => "La ciencia dentro de todo el ambiente",
 ];
 ?>
